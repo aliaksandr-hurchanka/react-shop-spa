a66b4d5114ee42dfab7f83bb0900a6a64c75b6a5
Andrey Astrouski <Andrey_Astrouski@epam.com>
2018-09-18 14:54:48 +0300

    Removes comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import Header from './components/common/Header'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './Routes'
 import Layout from './components/Layout'
-// import Catalog from './components/pages/Catalog'
-// import Error from './components/pages/Error'
 import './styles/imports.scss'
 
 const App = () => {
